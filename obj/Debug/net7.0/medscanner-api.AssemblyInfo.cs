3c19f2a93030bc4327bc23c2cec3ad2bb6082b6a
ppdfr84 <pedro.rocha@csjsistemas.com.br>
2025-01-12 21:55:45 -0300

    Atualização para o banco de dados remoto

--- obj/Debug/net7.0/medscanner-api.AssemblyInfo.cs
+++ obj/Debug/net7.0/medscanner-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("medscanner-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d81c8b277821b493ed03ec1b2b826d0b7e943a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1229168084032011b4e3abec2f3d08e3582099f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("medscanner-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("medscanner-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
