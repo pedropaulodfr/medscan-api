a2939967a3acb8274e654001fc2baeef64fcab3a
ppdfr84 <pedro.rocha@csjsistemas.com.br>
2025-01-19 10:06:50 -0300

    Ajuste gitignore

--- obj/Debug/net7.0/medscanner-api.AssemblyInfo.cs
+++ obj/Debug/net7.0/medscanner-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("medscanner-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4af6c19f0e02e6a3289ae835531ab1ab9613db8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d577143f4c11c3882105f91b9395d193dd5f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("medscanner-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("medscanner-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
