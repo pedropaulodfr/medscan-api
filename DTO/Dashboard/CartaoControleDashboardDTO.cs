093e0182a805366240e6db45be953373a76438b4
ppdfr84 <pedro.rocha@csjsistemas.com.br>
2025-01-30 20:01:19 -0300

    Atualiza .gitignore para ignorar pastas bin e obj

--- DTO/Dashboard/CartaoControleDashboardDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace authentication_jwt.DTO.Dashboard
-{
-    public class CartaoControleDashboardDTO
-    {
-        public DateTime? DataRetorno { get; set; }
-        public int? Quantidade { get; set; }
-    }
-}
